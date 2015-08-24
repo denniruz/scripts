25b294c644764e81b9929f512954d96073f0b7bc
Dennis <denniruz@gmail.com>
2015-08-24 10:28:36 -0400

    Scrape and backup all repos in a github organization

---
+++ backup_github_repos.sh
@@ -0,0 +1,17 @@
+#!/bib/bash 
+# Backup github repos in an organization
+#
+_CURL_CMD="/usr/bin/curl"
+_RUBY_CMD="/usr/bin/ruby"
+_ORGANIZATION="ArnoldMediaConsulting"
+#
+# Read in Username and Password
+echo -n " Username: "
+read _USERNAME
+echo -n " Password: "
+read _PASSWORD
+# 
+${_CURL_CMD} -u ${_USERNAME}:${_PASSWORD} -s https://api.github.com/orgs/${_ORGANIZATION}/repos?per_page=200 | ${_RUBY_CMD} -rubygems -e 'require json; JSON.load(STDIN.read). each { |repo| %x[git clone #{repo[ssh_url]} ]}' 2>&1 
+
+
+
